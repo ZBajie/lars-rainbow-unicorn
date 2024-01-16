abf8e06451cd720da7cdcffd0fcee39148d6ed9f
Lars Abelsson <georg483@msn.com>
2024-01-16 19:15:08 +0100

    second

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log("animals: ", animals);
+var volvo = {
+    color: "black",
+    year: 2000,
+    Rust: true,
+};
+console.log("volvo: ", volvo);
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
 const animals: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"]
 
 console.log("animals: ", animals)
+
+interface cars {
+  color: string
+  year: number
+  Rust: boolean
+}
+
+const volvo: cars = {
+  color: "black",
+  year: 2000,
+  Rust: true,
+}
+
+console.log("volvo: ", volvo)
